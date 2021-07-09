acf338e386f2fb2a1095d1828c5a0184d1b95197
Wenda Qin <wdqin@keller.bu.edu>
2021-07-09 15:34:50 -0400

    bash script repositioned

--- run/get_result.bash
+++ run/get_result.bash
@@ -4,8 +4,9 @@ flag="--dataset_dir output_data
       --batch_size 4
       --region_acceptance_score 0.8
       --i_r_threshold 0.2
+      --model_path ./testing_model
       --visualization_output_directory results/jpg
       --xml_output_directory results/xml
       "
 
-CUDA_VISIBLE_DEVICES=0 python3 eval.py $flag 
+CUDA_VISIBLE_DEVICES=0 python3 get_result.py $flag 
